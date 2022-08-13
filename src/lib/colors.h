057abae005b41f47614fb374d1fa5280df87691c
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-08-13 16:06:41 +0200

    Add lib/colors.h

---
+++ src/lib/colors.h
@@ -0,0 +1,37 @@
+/*
+ * Color definitions and functions for the terminal.
+ *
+ * Credits: https://gitlab.com/greggink/youtube_episode_terminal_control_2/-/blob/main/tc.h
+ */
+
+
+// Text color
+#define TC_NRM  "\x1B[0m"
+#define TC_RED  "\x1B[1;31m"
+#define TC_GRN  "\x1B[1;32m"
+#define TC_YEL  "\x1B[1;33m"
+#define TC_BLU  "\x1B[1;34m"
+#define TC_MAG  "\x1B[1;35m"
+#define TC_CYN  "\x1B[1;36m"
+#define TC_WHT  "\x1B[1;37m"
+
+// Bold colors
+#define TC_B_NRM  "\x1B[0m"
+#define TC_B_RED  "\x1B[0;31m"
+#define TC_B_GRN  "\x1B[0;32m"
+#define TC_B_YEL  "\x1B[0;33m"
+#define TC_B_BLU  "\x1B[0;34m"
+#define TC_B_MAG  "\x1B[0;35m"
+#define TC_B_CYN  "\x1B[0;36m"
+#define TC_B_WHT  "\x1B[0;37m"
+
+// Background colors
+#define TC_BG_NRM "\x1B[40m"
+#define TC_BG_RED "\x1B[41m"
+#define TC_BG_GRN "\x1B[42m"
+#define TC_BG_YEL "\x1B[43m"
+#define TC_BG_BLU "\x1B[44m"
+#define TC_BG_MAG "\x1B[45m"
+#define TC_BG_CYN "\x1B[46m"
+#define TC_BG_WHT "\x1B[47m"
+
