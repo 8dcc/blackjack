da7daa35efb915f3315d80af392d4f2872b329fe
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-08-31 01:06:55 +0200

    Fix characters for ncurses TODO: Add colors back

--- src/helpers.h
+++ src/helpers.h
@@ -4,6 +4,24 @@
 // =============================================================================
 
 char* n2str(int number) {
+#ifdef USE_NCURSES
+    switch (number) {
+        case 1:     return "A";     break;
+        case 2:     return "2";     break;
+        case 3:     return "3";     break;
+        case 4:     return "4";     break;
+        case 5:     return "5";     break;
+        case 6:     return "6";     break;
+        case 7:     return "7";     break;
+        case 8:     return "8";     break;
+        case 9:     return "9";     break;
+        case 10:    return "10";    break;
+        case 11:    return "J";     break;
+        case 12:    return "Q";     break;
+        case 13:    return "K";     break;
+        default:    break;
+    }
+#else
     switch (number) {
         case 1:     return TC_B_WHT "A"  TC_NRM;    break;
         case 2:     return TC_B_WHT "2"  TC_NRM;    break;
@@ -20,12 +38,21 @@ char* n2str(int number) {
         case 13:    return TC_B_WHT "K"  TC_NRM;    break;
         default:    break;
     }
+#endif
 
     return "";
 }
 
 char* s2str(int suit) {
-#ifdef SIMPLE_CHARS
+#ifdef USE_NCURSES              // TODO: ncurses colors
+    switch (suit) {
+        case DIAMONDS:      return "♦";     break;
+        case CLUBS:         return "♣";     break;
+        case HEARTS:        return "♥";     break;
+        case SPADES:        return "♠";     break;
+        default:            break;
+    }
+#elif SIMPLE_CHARS
     switch (suit) {
         case DIAMONDS:      return TC_B_RED "D" TC_NRM;     break;
         case CLUBS:         return TC_B_NRM "C" TC_NRM;     break;
